d1eb6bc0e987b749a3c3f8dc0902b6676e7998cf
Diego Escalante <diego.escalante01@gmail.com>
2022-04-03 12:23:27 -0600

    Add title scene

---
+++ Assets/Scripts/TitleBehavior.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleBehavior : MonoBehaviour {
+    public RawImage fadeImage;
+    
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            StartCoroutine(Begin());
+            enabled = false;
+        }
+    }
+
+    private IEnumerator Begin() {
+        float totalTime = 5f;
+        for (float timeElapsed = 0; timeElapsed < totalTime; timeElapsed += Time.deltaTime) {
+            fadeImage.color = Color.Lerp(Color.clear, Color.black, timeElapsed / totalTime);
+            yield return null;
+        }
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
+    }
+}
